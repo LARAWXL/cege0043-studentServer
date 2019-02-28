508a8fedc272a6f8919ee8274b971c91d95049ea
Xiaolu Wang <xiaolu.wang.18@ucl.ac.uk>
2019-02-28 11:33:04 +0000

    Modify the code so that the server returns test.html when it is requested

--- studentServer.js
+++ studentServer.js
@@ -9,8 +9,19 @@ var app = express();
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
+
+
 app.get('/', function (req, res) {
     res.send("hello world from the HTTP server");
+});
+
+// Modify the code so that the server returns test.html
+// when it is requested
+app.get('/test.html', function (req, res) {
+    // run some server-side code
+    console.log('test.html requested');
+    // note that __dirname gives the path to the studentServer.js file
+    res.sendFile(__dirname + '/test.html');
 });
 
 // adding functionality to log the requests
