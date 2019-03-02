38518b12bb01accfa0bf9be4b8f6a798140dc2ab
Xiaolu Wang <xiaolu.wang.18@ucl.ac.uk>
2019-03-02 21:17:20 +0000

    modify the code for making requests for data from this server via another server (the PhoneGap server).

--- studentServer.js
+++ studentServer.js
@@ -3,6 +3,15 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+// modify the code for to “cross origin request”
+// which means making requests for data from this server
+// via another server (the PhoneGap server).
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files
 // if they are not directly called in a typed URL
@@ -15,14 +24,7 @@ app.get('/', function (req, res) {
     res.send("hello world from the HTTP server");
 });
 
-// modify the code for to “cross origin request”
-// which means making requests for data from this server
-// via another server (the PhoneGap server).
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
+
 
 
 // adding functionality to log the requests
